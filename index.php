bd7f47d0e011a3f7603ecc2ab2f3a8c7b1729cde
AmNetDT <amnetdt@yahoo.com>
2021-07-08 09:21:28 +0100

    Update

--- index.php
+++ index.php
@@ -1,38 +1,256 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <!-- Favicon icon -->
-  <link rel="icon" type="image/png" sizes="16x16" href="src/assets/images/favicon.png" />
-  <title>RILA</title>
-  <link href="src/assets/node_modules/bootstrap/css/bootstrap.min.css" rel="stylesheet" />
-  <link href="src/css/style.css" rel="stylesheet" />
+	<title>MyEdgyDeal</title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+	<link href="https://fonts.googleapis.com/css?family=Montserrat:200,300,400,500,600,700,800&display=swap" rel="stylesheet">
+
+	<link href="https://fonts.googleapis.com/css?family=Montserrat:200,300,400,500,600,700,800&display=swap" rel="stylesheet">
+
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+
+	<link rel="stylesheet" href="css/animate.css">
+
+	<link rel="stylesheet" href="css/owl.carousel.min.css">
+	<link rel="stylesheet" href="css/owl.theme.default.min.css">
+	<link rel="stylesheet" href="css/magnific-popup.css">
+
+	<link rel="stylesheet" href="css/flaticon.css">
+	<link rel="stylesheet" href="css/style.css">
+	<link rel="stylesheet" href="css/personal.css">
+
 </head>
+
 <body>
-  <div class="container">
-    <div class="row justify-content-center py-5">
-         
-          <div class="col-sm-4 pt-3 border radius">
-            
-            <div class="card-body px-4 mb-5 text-center">
-              
-              <img src="src/assets/images/logo.png" alt="logo" class="logo" />
-              <p class="login-card-description mt-2">
-                <a href="login/index">
-              Login to your account</a></p>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-   
-    </div>
-        <footer class="footer fixed-bottom text-center text-dark ">
-            &copy; Redeemer's International Leadership Academy - RILA 
-            <script>document.write(new Date().getFullYear());</script> 
-        </footer>
+
+	<header>
+		<!-- Beginning nav -->
+
+
+		<nav class="navbar fixed-top navbar-expand-lg navbar-light font-weight-bold">
+			<div class="container">
+				<a class="navbar-brand" href="index.php">
+					<!--img src="img/logo-small.png" alt="MyEdgyDeal" class="img-fluid"!-->
+					<span class="fa fa-bus text-darkcyan"></span> MyEdgyDeal
+				</a>
+				<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+					<span class="navbar-toggler-icon"></span>
+				</button>
+				<div class="collapse navbar-collapse" id="navbarNavDropdown">
+					<div class="col-md-7 col-sm-6">
+						<ul class="navbar-nav m-auto">
+							<li class="nav-item dropdown">
+								<a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+									Company
+								</a>
+								<div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+									<div class="row justify-content-center">
+										<div class="col-md-2 col-sm-6">
+											<span style="font-size: 5rem;">
+												<span class="fa fa-bus"></span></span>
+
+
+										</div>
+										<div class="col-md-4 col-sm-6">
+											<a class="dropdown-item" href="domain-search.php">About Us</a>
+											<a class="dropdown-item" href="#">Why choose MyEdgyDeal</a>
+											<a class="dropdown-item" href="#">Our Service</a>
+											<a class="dropdown-item" href="#">Success Rate</a>
+										</div>
+									</div>
+								</div>
+							</li>
+							<li class="nav-item">
+								<a class="nav-link" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+									Customers
+								</a>
+
+							</li>
+							<li class="nav-item">
+								<a class="nav-link" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+									Partners
+								</a>
+
+							</li>
+							<li class="nav-item">
+								<a class="nav-link" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+									Help
+								</a>
+							</li>
+
+						</ul>
+					</div>
+					<div class="col-md-5 col-sm-6">
+						<div class="justify-content-left">
+							<ul class="navbar-nav m-auto">
+								<li>
+									<button type="button" class="customised-button-white"><span class="fa fa-map-marker"></span> Coverage</button>
+								</li>
+								<li class="nav-item">
+									<button type="button" class="customised-button"><span class="fa fa-user-o"></span> Log in</button>
+								</li>
+								<li class="nav-item">
+									<button type="button" class="customised-button"><span class="fa fa-user-o"></span> Create account</button>
+								</li>
+							</ul>
+
+
+
+						</div>
+					</div>
+				</div>
+			</div>
+		</nav>
+
+		<!-- END nav -->
+	</header>
+
+
+	<section class="ftco-section bg-light">
+		<div class="container">
+			<div class="row justify-content-center py-5 my-3">
+				<div class="col-md-7 heading-section text-center ftco-animate fadeInUp ftco-animated">
+					<!-- span class="subheading">Price &amp; Plans</span!-->
+					<h1>The fast, affordable way to deliver.</h1>
+					<p>Choose one of the package that path to your chioce.</p>
+				</div>
+			</div>
+			<div class="row justify-content-center pb-5 mb-3">
+				<div class="col-md-6 col-lg-5 ftco-animate fadeInUp ftco-animated">
+					<div class="block-7">
+						<div class="text-center">
+
+							<p>
+								<span class="excerpt">Deliver an Item </span>
+								<button type="button" class="customised-button"><span class="fa fa-shopping-basket"></span> Request</button>
+								<span>Create Account</span>
+							</p>
+
+
+
+						</div>
+					</div>
+				</div>
+				<div class="col-md-6 col-lg-5 ftco-animate fadeInUp ftco-animated">
+					<div class="block-7">
+						<div class="text-center">
+							<p>
+								<span class="excerpt">Driver or Rider </span>
+								<button type="button" class="customised-button"><span class="fa fa-bus"></span> Apply</button>
+								<span>Create Account</span>
+							</p>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="row justify-content-center pb-5 mb-3">
+				<div class="col-md-6 col-lg-5 ftco-animate fadeInUp ftco-animated">
+					<div class="block-7">
+						<div class="text-center">
+							<p>
+								<span class="excerpt">Business &amp; Partners</span>
+								<button type="button" class="customised-button"><span class="fa fa-handshake-o"></span> Sign up</button>
+
+							</p>
+						</div>
+					</div>
+				</div>
+
+			</div>
+		</div>
+	</section>
+
+	<section class="ftco-section bg-light ftco-no-pt">
+		<div class="container">
+			<div class="row justify-content-center mb-3">
+				<div class="col-md-9 heading-section heading-section-white text-center ftco-animate pt-5">
+					<h1 class="text-dark">Here’s why we’re different</h1>
+					<p class="text-dark">Connecting cities and communities with shared mobility. We are the largest shared mobility startup in Nigeria and we offer commuters three basic values</p>
+				</div>
+			</div>
+
+
+	</section>
+	<footer class="footer">
+		<div class="container px-lg-5">
+			<div class="row">
+				<div class="col-md-12 py-5">
+					<div class="row justify-content-center">
+						<div class="col-md-3 mb-md-0 mb-4 justify-content-center">
+							<p class="footer-heading" style="font-size: 10rem; margin-top:-80px;"><span class="fa fa-bus"></span></p>
+						</div>
+						<div class=" col-md-3 mb-md-0 mb-4">
+							<h5 class="footer-heading">Company</h5>
+							<ul class="list-unstyled">
+								<li><a href="#" class="py-1">Documentation</a></li>
+								<li><a href="#" class="py-1">API Reference</a></li>
+							</ul>
+
+						</div>
+						<div class="col-md-3 mb-md-0 mb-4">
+							<h5 class="footer-heading">Products</h5>
+							<ul class="list-unstyled">
+								<li><a href="#" class="py-1">Documentation</a></li>
+								<li><a href="#" class="py-1">API Reference</a></li>
+							</ul>
+							<p><span style="font-weight:500"><a href="#">Follow Us </a></span></p>
+							<ul class="ftco-footer-social p-0">
+								<li class="ftco-animate"><a href="#" data-toggle="tooltip" data-placement="top" title="Twitter"><span class="fa fa-twitter"></span></a></li>
+								<li class="ftco-animate"><a href="#" data-toggle="tooltip" data-placement="top" title="Facebook"><span class="fa fa-facebook"></span></a></li>
+								<li class="ftco-animate"><a href="#" data-toggle="tooltip" data-placement="top" title="Instagram"><span class="fa fa-linkedin"></span></a></li>
+								<li class="ftco-animate"><a href="#" data-toggle="tooltip" data-placement="top" title="Instagram"><span class="fa fa-instagram"></span></a></li>
+							</ul>
+						</div>
+
+
+					</div>
+				</div>
+			</div>
+		</div>
+
+		<div class="row">
+			<div class="container px-lg-5">
+				<div class="col-md-12 p-3 m-0">
+					<p class="copyright" style="color:#ffffff">
+
+						Copyright &copy;<script>
+							document.write(new Date().getFullYear());
+						</script> All rights reserved <a href="http://myedgydeal.com">MyEdgyDeal</a>
+					</p>
+				</div>
+			</div>
+		</div>
+	</footer>
+
+
+
+	<!-- loader -->
+	<div id="ftco-loader" class="show fullscreen">
+		<svg class="circular" width="48px" height="48px">
+			<circle class="path-bg" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke="#eeeeee" />
+			<circle class="path" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke-miterlimit="10" stroke="#F96D00" />
+		</svg>
+	</div>
+
+
+	<script src="js/jquery.min.js"></script>
+	<script src="js/jquery-migrate-3.0.1.min.js"></script>
+	<script src="js/popper.min.js"></script>
+	<script src="js/bootstrap.min.js"></script>
+	<script src="js/jquery.easing.1.3.js"></script>
+	<script src="js/jquery.waypoints.min.js"></script>
+	<script src="js/jquery.stellar.min.js"></script>
+	<script src="js/jquery.animateNumber.min.js"></script>
+	<script src="js/owl.carousel.min.js"></script>
+	<script src="js/jquery.magnific-popup.min.js"></script>
+	<script src="js/scrollax.min.js"></script>
+	<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBVWaKrjvy3MaE7SQ74_uJiULgl1JY0H2s&sensor=false"></script>
+	<script src="js/google-map.js"></script>
+	<script src="js/main.js"></script>
 
 </body>
+
 </html>
